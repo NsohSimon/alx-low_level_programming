bf96a396c94029d79e5228b5bcfb6c1c84ee2fc9
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-17 03:03:01 -0800

    Fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) /*Printing a text*/
+int main(void) /*Printing a text*/ /*main=entry point*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
