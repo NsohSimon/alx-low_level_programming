8e60815b251cc2229863b7fe470a1182cb2378bf
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-18 15:25:06 -0800

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
 	  {
-	    printf("%d\n is positive");
+	    printf("%lu\n is positive");
 	  }
 	else if (n=0)
 	  {
-	    printf("%d\n is zero");
+	    printf("%lu\n is zero");
 	  }
 	else
 	  {
-	    printf("%d\n is negative");
+	    printf("%lu\n is negative");
 	  }
 	return (0);
 }
