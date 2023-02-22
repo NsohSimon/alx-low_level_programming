2d81cdf19dc99e6d2904895f4caf07f436c87ef1
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-21 19:30:23 -0800

    Prints alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void)-prints alphabets 10 times
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+int t = 1;
+while (t <= 10; t++)
+{
+void print_alphabet(void);
+}
+}
