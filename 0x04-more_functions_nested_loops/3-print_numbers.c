7da02bcfa5b4e458f428fe8e6a334a1a5fb1afc3
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-24 16:21:32 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers- print numbers 1 to 9
+ * Return: always zero
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
