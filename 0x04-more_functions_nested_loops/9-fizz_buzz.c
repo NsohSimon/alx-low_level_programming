cf5bf7fb2d7e4aace77a283261f4057a664c6ca6
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-25 05:02:53 -0800

    Print with exception

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int main(void)
+{
+int n;
+for (n >= 1; n <= 100; n++)
+{
+if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (n % 3 == 0 && n % 5 == 0)
+{
+print("FizzBuzz ");
+}
+else
+{
+printf("%d ",n);
