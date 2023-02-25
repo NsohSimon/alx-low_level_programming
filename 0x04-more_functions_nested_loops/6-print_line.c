049d078f19c9aeabad206d8a96ae796e54137892
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-24 17:14:04 -0800

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line-prints a line
+ * Return: always 0
+ */
+void print_line(int n)
+{
+if (n <= 0)
+_putchar('\n');
+else
+{
+int m;
+for (m = 1; m <= n; m++)
+_putchar('_');
+}
+_putchar('\n');
+}
