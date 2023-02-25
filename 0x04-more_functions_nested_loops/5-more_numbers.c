ad89c2a12ab2c05e9ca31a4ebd52271d132eaa12
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-24 17:03:00 -0800

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,10 +5,15 @@
  */
 void more_numbers(void)
 {
-for (int n = 1; n <= 10; c++)
+int i, j;
+for (i = 1; i <= 10; i++)
 {
-for (int m = 0; m <= 14; m++)
-_putchar(m);
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('1');
+_putchar (j % 10 + '0');
 }
 _putchar('\n');
+}
 }
