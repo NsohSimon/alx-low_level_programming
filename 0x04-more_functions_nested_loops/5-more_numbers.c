b53551640616368ee03edfa4b29e237f1e323a2c
Nsoh Simon Asore <nsohsimonasore@gmail.com>
2023-02-24 16:55:59 -0800

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * more_numbers-print numbers 10 times
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+for (int n = 1; n <= 10; c++)
+{
+for (int m = 0; m <= 14; m++)
+_putchar(m);
+}
+_putchar('\n');
+}
